d2a49ac39cfd25f147e2afa07519f3fcb168ef87
Oceanshy12-YANG <152771172+Oceanshy12-YANG@users.noreply.github.com>
2024-03-13 10:38:34 +0800

    Update swinir (5).py

--- model/swinir (5).py
+++ model/swinir (5).py
@@ -948,6 +948,11 @@ class SwinIR(pl.LightningModule, ImageLoggerMixin):
         hq, lq = batch[self.hq_key], batch[self.lq_key]
         hq = rearrange(((hq + 1) / 2).clamp_(0, 1), "n h w c -> n c h w")
         lq = rearrange(lq, "n h w c -> n c h w")
+        images, labels = next(iter(lq))
+        print(images.size())  # torch.Size([8, 1, 28, 28])
+        images = make_grid(images, 4, 0)
+        print(images.size())  # torch.Size([3, 84, 84])
+        save_image(images, 'D:\maozan1\Desktop\JDWork\\vscode\pytorch-demo\\test.jpg')
         pred = self(lq)
         loss = self.get_loss(pred, hq)
         self.log("train_loss", loss, on_step=True)
@@ -996,7 +1001,7 @@ class SwinIR(pl.LightningModule, ImageLoggerMixin):
 
 
 
-
+"""
     def lq_images(self, batch: Any) -> Dict[str, torch.Tensor]:
         hq, lq = batch[self.hq_key], batch[self.lq_key]
         hq = rearrange(((hq + 1) / 2).clamp_(0, 1), "n h w c -> n c h w")
